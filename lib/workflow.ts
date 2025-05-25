d18526b44f549d58f5699bccfa98d8e4cab3951b
Samarth Kamble <kamblesamarth04@gmail.com>
2025-05-25 23:01:26 +0530

    Implement the upstash qstash for user state emailing

---
+++ lib/workflow.ts
@@ -0,0 +1,23 @@
+import { Client as Qstash, resend } from "@upstash/qstash";
+import { Client as WorkflowClient } from "@upstash/workflow";
+
+import config from "./config";
+
+export const workflowClient = new WorkflowClient({
+  baseUrl: config.env.upstash.qstashUrl,
+  token: config.env.upstash.qstashToken!,
+});
+
+export const qstashClient = new Qstash({
+  token: config.env.upstash.qstashToken!,
+});
+
+export const sendEmail = async ({
+  email,
+  subject,
+  message,
+}: {
+  email: string;
+  subject: string;
+  message: string;
+}) => {};
